20ad2413bdb082cb4f23df3f78ca614b082ae481
Jekaterina Berkovica <jekaterina.berkovich@gmail.com>
2023-07-19 15:57:05 +0300

    Update todo for Vincent

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,10 @@
-// TODO: please implement function to read CSV data and return values
-// same values as we have at GNL tables
-// @vincent
+// TODO: please implement function to read CSV data and return values similar to GNL 
+// Assuming it will take arguments Level+Module name
+// Assignee: @vincent
+
+// TODO: process CSV to some object maybe?
+
+function calculateAverageDifficulty(module) {
+    // do something with data
+    // return difficulty as string
+}
