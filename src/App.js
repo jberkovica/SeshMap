d2dc6963b776ed073aa798f794507ae619748cd1
Jekaterina Berkovica <jekaterina.berkovich@gmail.com>
2023-08-02 17:20:58 +0300

    NavBar initial implementation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import "./App.css";
 
 function App() {
     return (
-        <div classNameName="App">
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="App">
+            <nav className="navbar navbar-expand-lg navbar-custom">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">
-                        Navbar
+                        <img
+                            src="./../public/seshmap-logo.png"
+                            alt="Logo"
+                            className="navbar-logo"
+                        />
                     </a>
                     <button
                         className="navbar-toggler"
@@ -26,73 +30,31 @@ function App() {
                     >
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a
-                                    className="nav-link active"
-                                    aria-current="page"
-                                    href="#"
-                                >
+                                <a className="nav-link active" href="#">
                                     Home
                                 </a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="#">
-                                    Link
+                                    Session Planner
                                 </a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a
-                                    className="nav-link dropdown-toggle"
-                                    href="#"
-                                    role="button"
-                                    data-bs-toggle="dropdown"
-                                    aria-expanded="false"
-                                >
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li>
-                                        <a className="dropdown-item" href="#">
-                                            Action
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a className="dropdown-item" href="#">
-                                            Another action
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <hr className="dropdown-divider"></hr>
-                                    </li>
-                                    <li>
-                                        <a className="dropdown-item" href="#">
-                                            Something else here
-                                        </a>
-                                    </li>
-                                </ul>
                             </li>
                             <li className="nav-item">
-                                <a
-                                    className="nav-link disabled"
-                                    aria-disabled="true"
-                                >
-                                    Disabled
+                                <a className="nav-link" href="#">
+                                    Resources
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
+                                    Ranking
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
+                                    Survey
                                 </a>
                             </li>
                         </ul>
-                        <form className="d-flex" role="search">
-                            <input
-                                className="form-control me-2"
-                                type="search"
-                                placeholder="Search"
-                                aria-label="Search"
-                            ></input>
-                            <button
-                                className="btn btn-outline-success"
-                                type="submit"
-                            >
-                                Search
-                            </button>
-                        </form>
                     </div>
                 </div>
             </nav>
