d6e9549e148cf46e71b9085844811bf0dc1365cb
Shikhar-S1ngh <140382518+Shikhar-S1ngh@users.noreply.github.com>
2023-08-16 17:42:22 +0530

    Carousel Implemented
    Implemented FAQ carousel, added a separate component for it along with a CSS file.
    Week and Acknowledgements implementation implemented.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,50 @@
 import React from "react";
+import Q_Carousel from "./Q_Carousel.js"
+
 
 function Home() {
+
+    
+// Week identification code extracted from Denis Bakunovitch's
+// "what week is it" web application
+
+function weeklee() {
+    //const dateFrom = new Date('2020-04-20');
+    //const dateFrom = new Date('2020-10-12');
+    //const dateFrom = new Date('2021-04-12');
+    //const dateFrom = new Date('2021-10-11');
+    //const dateFrom = new Date('2022-04-04');
+    //const dateFrom = new Date('2022-10-10');
+    const dateFrom = new Date('2023-04-17');
+    
+  
+    const dateTo = new Date();
+    let w = Math.ceil((dateTo - dateFrom)/(1000*60*60*24*7));
+  
+    if(w < 1 || w > 22) { w = '∞'; }
+    console.log(w);
+    return w;
+  
+};
+
+
     return (
-        // TODO: implement grid
+        <>
         <div>
-            <div className="bg-secondary bg-opacity-25 p-5 rounded-lg">
-                <h1 className="display-4">Welcome to SeshMap</h1>
-                <p className="lead">
-                    This is a simple hero unit, a simple jumbotron-style component
-                    for calling extra attention to featured content or information.
-                </p>
-                <hr className="my-4"></hr>
+            <div className="bg-secondary bg-opacity-25 p-5 rounded-lg text-left">
+                <h2 className="display-6">Welcome to SeshMap!</h2>
                 <p>
-                    It uses utility classNamees for typography and spacing to space
-                    content out within the larger container.
+                    SeshMap is your one-stop resource and planning hub, for all things<br></br>
+                    related to the online University of London BSc. Computer Science degree!
                 </p>
-                <a className="btn btn-primary btn-lg" href="/" role="button">
-                    Learn more
-                </a>
+                <p>
+                    All information related to modules can be found across these pages; you may also browse <br></br>
+                    through course-specific data to make informed decisions about your next session.
+                </p>
             </div>
+            <Q_Carousel />
         </div>
+        </>
     );
 }
 
