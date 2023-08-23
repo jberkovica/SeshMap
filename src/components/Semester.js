bdb7f925881cd66d1875467ba0d030bf21f786e5
Jekaterina Berkovica <jekaterina.berkovich@gmail.com>
2023-08-23 15:17:38 +0300

    Implement Session and Module components

--- src/components/Semester.js
+++ src/components/Semester.js
@@ -1,25 +1,80 @@
 import React from "react";
+import "./Semester.css";
+import Module from "./Module";
 
-const Semester = () => {
-    return (
-        <div>
-            <ModuleSelection />
-        </div>
-    );
-};
+function Semester() {
+    const headers = [
+        "Difficulty",
+        "Time",
+        "Quality",
+        "Self-Study",
+        "Learning",
+        "Interest",
+        "Midterm",
+        "Final",
+    ];
 
-const ModuleSelection = () => {
     return (
-        <div className="card">
-            <div className="col">
-                <div className="row">Easy</div>
-                <div className="row">4-6</div>
-                <div className="row">Good</div>
-                <div className="row">Some</div>
-                <div className="row">Interesting</div>
+        < div className="mb-5">
+            <div className="row">
+                <div className="col-10">
+                    <h4 className="semester-title mb-5 mt-5 text-start">
+                        Semester 1
+                    </h4>
+                    <div className="semester-container p-4">
+                        <div className="row">
+                            <div className="col-2">
+                                {headers.map(header => (
+                                    <p key={header} className="mb-3 text-white">
+                                        {header}
+                                    </p>
+                                ))}
+                            </div>
+                            <div className="col-10">
+                                <div className="row">
+                                    <div className="col position-relative">
+                                        <Module />
+                                    </div>
+                                    <div className="col position-relative">
+                                        <Module />
+                                    </div>
+                                    <div className="col position-relative">
+                                        <Module />
+                                    </div>
+                                    <div className="col position-relative">
+                                        <Module />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-2">
+                    <h4 className="semester-title mb-5 mt-5 text-start">Summary</h4>
+                    <SemesterSummary />
+                </div>
             </div>
         </div>
     );
-};
+}
+
+function SemesterSummary() {
+    return (
+        <div className="summary-container">
+            <div className="module-container p-3">
+                <div className="p-3">
+                    <p className="module-data mb-3">Easy</p>
+                    <p className="module-data mb-3">4-6</p>
+                    <p className="module-data mb-3">Good</p>
+                    <p className="module-data mb-3">Some</p>
+                    <p className="module-data mb-3">Good amount</p>
+                    <p className="module-data mb-3">Interesting</p>
+                    <p className="module-data mb-3">Projects (50%)</p>
+                    <p className="module-data mb-3">Projects (50%)</p>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 export default Semester;
--- src/pages/SessionPlanner.js
+++ src/pages/SessionPlanner.js
@@ -5,46 +5,58 @@ function SessionPlanner() {
     return (
         <div>
             <div className="bg-secondary bg-opacity-25 p-5 rounded-lg">
-                <h3 className="display-5 mb-5">Tips on how to plan your semester</h3>
-                <div>
-                    <ul>
-                        <li className="my-3">
-                            Have a look through the statistics and data collected
-                            from previous students.
-                        </li>
-                        <li className="my-3">
-                            Consider the time constraints, level of difficulty and
-                            module quality before selecting a combination for your
-                            next semester.
-                        </li>
-                        <li className="my-3">
-                            Remember to also to search and ask questions on the Slack
-                            channels related to each module!
-                        </li>
-                        <li className="my-3">
-                            Keep in mind progressional modules, i.e., the core
-                            modules you have to complete and pass in order to
-                            progress to next Level.
-                        </li>
-                        <li className="my-3">
-                            You have a minimum of 3 years to a maximum of 6 years to
-                            complete the degree.
-                        </li>
-                        <li className="my-3">
-                            You are permitted to take at minimum 2 modules per
-                            session and maximum 4 modules, excluding re-sits and RPL.
-                        </li>
-                        <li className="my-3">
-                            For further information visit the program regulation
-                            link:{" "}
-                            <a href="https://www.london.ac.uk/sites/default/files/regulations/progegs-bsc-computer-science-2023-24.pdf">
-                                Regulations 2023-2024
-                            </a>
-                        </li>
-                    </ul>
+                <div className="row">
+                    <div className="col-4">
+                        <h5 className="display-5 mb-5">
+                            Tips on how to plan your semester
+                        </h5>
+                    </div>
+                    <div className="col-8">
+                        <ul>
+                            <li className="my-3">
+                                Have a look through the statistics and data collected
+                                from previous students.
+                            </li>
+                            <li className="my-3">
+                                Consider the time constraints, level of difficulty
+                                and module quality before selecting a combination for
+                                your next semester.
+                            </li>
+                            <li className="my-3">
+                                Remember to also to search and ask questions on the
+                                Slack channels related to each module!
+                            </li>
+                            <li className="my-3">
+                                Keep in mind progressional modules, i.e., the core
+                                modules you have to complete and pass in order to
+                                progress to next Level.
+                            </li>
+                            <li className="my-3">
+                                You have a minimum of 3 years to a maximum of 6 years
+                                to complete the degree.
+                            </li>
+                            <li className="my-3">
+                                You are permitted to take at minimum 2 modules per
+                                session and maximum 4 modules, excluding re-sits and
+                                RPL.
+                            </li>
+                            <li className="my-3">
+                                For further information visit the program regulation
+                                link:{" "}
+                                <a href="https://www.london.ac.uk/sites/default/files/regulations/progegs-bsc-computer-science-2023-24.pdf">
+                                    Regulations 2023-2024
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-            <Semester />
+            <div className="container">
+                <Semester />
+                <Semester />
+                <Semester />
+            </div>
+
             <div className="d-flex justify-content-center align-items-center mt-5">
                 <i className="bi bi-plus-circle fs-3 text-primary"></i>
             </div>
