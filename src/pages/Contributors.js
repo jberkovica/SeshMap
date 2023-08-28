814b76e72dee78548ea6a2d4c4cd4b3816f37d73
Shikhar-S1ngh <140382518+Shikhar-S1ngh@users.noreply.github.com>
2023-08-29 00:17:48 +0530

    Home Page implemented, links to Github pending
    Added functions to automate name collection and display for the names. Carousel format needs some tweaking.

---
+++ src/pages/Contributors.js
@@ -0,0 +1,48 @@
+const Contributors =
+[
+ // Table 1   
+    {name: "Kate", link: "https://github.com/jberkovica"},
+    {name: "Rizwana", link: "" },
+    {name: "Vincent", link: "https://github.com/sithvincent" },
+    {name: "Franz", link: "https://github.com/FranzMath"},
+    {name: "Shikhar", link: "https://github.com/Shikhar-S1ngh"},
+
+    {name: "Sergei", link: ""},
+    {name: "Gerry L", link: ""},
+    {name: "Marina", link :""},
+    {name: "Ying", link: ""},
+    {name: "Ankush", link: ""},
+
+    {name: "REPL Contributors", link: "https://github.com/world-class/REPL#contributors"},
+    {name: "Irfan", link: ""},
+    {name: "Olga", link: ""},
+    {name: "Stacy", link: ""},
+    {name: "Florent", link: ""},
+ // Table 2
+    {name: "Danny Callaghan", link: ""},
+    {name: "Jamie Jackson", link: ""},
+    {name: "Sébastien Lavoie", link: ""},
+    {name: "Andrés Cruz", link: ""},
+    {name: "Zach Chan", link: ""},
+    
+    {name: "Felipe Balbi", link: ""},
+    {name: "Alex Flach", link: ""},
+    {name: "Jibin Joseph", link: ""},
+    {name: "John Gerhardt", link: ""},
+    {name: "Dan Capener", link: ""},
+    
+    {name: "Ahmed Hassan", link: ""},
+    {name: "Janette", link: ""},
+    {name: "Alexeia Wiesner", link: ""},
+    {name: "Petra", link: ""},
+    {name: "Ivan Kruger", link: ""},
+ // Table 3
+    {name: "Salman", link: ""},
+    {name: "cprieto", link: ""},
+    {name: "Aimee", link: ""},
+    {name: "Blair", link: ""},
+    {name: "Winfred", link: ""}
+
+];
+
+module.exports = {Contributors};
