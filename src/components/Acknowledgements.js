8431d5b3c47238e9024a5838095ad8e14d727d3a
Jekaterina Berkovica <jekaterina.berkovich@gmail.com>
2023-08-30 16:30:46 +0300

    home page implementation

---
+++ src/components/Acknowledgements.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Contributors from "../contributors.js";
+import { ReactComponent as Person } from "../assets/person.svg";
+import "./Acknowledgements.css";
+
+const nameRows = [];
+const nameTables = [];
+
+const buildNameRow = index => {
+    // 5 names show up in each acknowledgement row.
+    // This function takes the index of
+    // a name, then builds a row
+    var rowData = [];
+    for (var i = index; i < index + 5; ++i) {
+        rowData.push(
+            <td>
+                <Person /> <a href={Contributors[i].link}>{Contributors[i].name}</a>
+            </td>
+        );
+    }
+    nameRows.push(<tr> {rowData}</tr>);
+};
+
+// Builds table from the list of rows generated using buildNameRow.
+// Basically, 3 items in nameRows constitute a table, so we need
+// only call this function with the right values.
+const buildTable = index => {
+    var rows = [];
+    for (var i = index; i < index + 3; ++i) {
+        rows.push(nameRows[i]);
+    }
+    nameTables.push(<table class="Names">{rows}</table>);
+};
+
+function A_Carousel() {
+    for (let i = 0; i < Contributors.length; ++i) {
+        // Build a name row with next 5 names, current name included.
+        if (i % 5 === 0) {
+            buildNameRow(i);
+        }
+    }
+
+    // Build tables and add to the nameTable array.
+    for (let i = 0; i < nameRows.length; ++i) {
+        if (i % 3 === 0) {
+            buildTable(i);
+        }
+    }
+
+    return (
+        <>
+            <Carousel className="Acks">
+                <Carousel.Item interval={null}>
+                    <div>{nameTables[0]}</div>
+                </Carousel.Item>
+                <Carousel.Item interval={null}>
+                    <div>{nameTables[1]}</div>
+                </Carousel.Item>
+                <Carousel.Item interval={null}>
+                    <div>{nameTables[2]}</div>
+                </Carousel.Item>
+            </Carousel>
+        </>
+    );
+}
+
+export default A_Carousel;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,68 @@
 import React from "react";
+import Q_Carousel from "../components/QuestionsCarousel.js";
+import A_Carousel from "../components/Acknowledgements.js";
+import "./Home.css";
+
+// currentWeek code is taken from Denis Bakunovitch's "what week is it anyway"
+// web application.
+
+function currentWeek() {
+    //const dateFrom = new Date('2020-04-20');
+    //const dateFrom = new Date('2020-10-12');
+    //const dateFrom = new Date('2021-04-12');
+    //const dateFrom = new Date('2021-10-11');
+    //const dateFrom = new Date('2022-04-04');
+    //const dateFrom = new Date('2022-10-10');
+    const dateFrom = new Date("2023-04-17");
+
+    const dateTo = new Date();
+    let w = Math.ceil((dateTo - dateFrom) / (1000 * 60 * 60 * 24 * 7));
+
+    if (w < 1 || w > 22) {
+        w = 0;
+    }
+
+    return w;
+}
 
 function Home() {
+    const Week = currentWeek();
+
     return (
-        // TODO: implement grid
-        <div>
-            <div className="bg-secondary bg-opacity-25 p-5 rounded-lg">
-                <h1 className="display-4">Welcome to SeshMap</h1>
-                <p className="lead">
-                    This is a simple hero unit, a simple jumbotron-style component
-                    for calling extra attention to featured content or information.
-                </p>
-                <hr className="my-4"></hr>
-                <p>
-                    It uses utility classNamees for typography and spacing to space
-                    content out within the larger container.
-                </p>
-                <a className="btn btn-primary btn-lg" href="/" role="button">
-                    Learn more
-                </a>
+        <>
+            <div>
+                <div className="bg-secondary bg-opacity-25 p-5 rounded-lg text-left">
+                    <div className="Main">
+                        <div>
+                            <h1>Welcome to SeshMap!</h1>
+                            <p>
+                                SeshMap is your one-stop resource and planning hub,
+                                for all things<br></br>
+                                related to the online University of London BSc.
+                                Computer Science degree!
+                            </p>
+                            <p>
+                                All information related to modules can be found
+                                across these pages; you may also browse <br></br>
+                                through course-specific data to make informed
+                                decisions about your next session.
+                            </p>
+                        </div>
+                        <div className="Week">
+                            <h4 className="display-6">week</h4>
+                            {Week}
+                        </div>
+                    </div>
+                </div>
+                <Q_Carousel />
+                <div className="bg-secondary bg-opacity-25">
+                    <div className="Title">
+                        <h3>Acknowledgements</h3>
+                    </div>
+                    <A_Carousel />
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
