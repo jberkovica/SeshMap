8cf0fae3a6f7344dad41d2af6dcec06433fe02b8
Jekaterina Berkovica <jekaterina.berkovich@gmail.com>
2023-09-06 22:33:33 +0300

    Implement Session Planner

---
+++ src/components/SemesterSummary.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./SemesterSummary.css";
+// import { getModuleValueByCategory } from "../helpers";
+// import { findModuleByName } from "../modules";
+
+function SemesterSummary() {
+
+
+    return (
+        <div className="summary-container px-4 pt-4">
+            <div className="p-3">
+                <p className="module-data mb-3">Total time: </p>
+
+            </div>
+        </div>
+    );
+}
+
+export default SemesterSummary;
